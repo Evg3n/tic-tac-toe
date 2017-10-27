8638990c718e77f89ca0aa9bd147653c30a4474d
Yauheni Yudzitski <yauheni.yudzitski@gmail.com>
2017-10-27 10:11:34 -0400

    Initial commit

---
+++ src/main/java/com/rubiconproject/tictactoe/Main.java
@@ -0,0 +1,42 @@
+package com.rubiconproject.tictactoe;
+
+/**
+ * Created by Yauheni Yudzitski <yauheni.yudzitski@gmail.com>
+ * <p>
+ * JAVA CODING TASK: Implement standard game of TicTacToe
+ * - two players: X and O
+ * - Players alternate in turn (as in standard TicTacToe)
+ * - 3x3 board
+ * - Default "Win Conditions":  3 in a row, a column or diagonally
+ * <p>
+ * Further Directives
+ * - Simple UI:  stdout text prompt to get the (x,y) for each move from stdin.
+ * Keep UI simple and dumb as possible while still
+ * letting players choose their moves.
+ * UI will be used primarily for demonstration/validation.
+ * The UI is not the focus of this task.
+ * <p>
+ * - Choose a design that would allow easy addition of
+ * alternate games where just the "win conditions" would change.
+ * The size/shape of the board and the order of play do not change
+ * with the diffferent versions of the game...  just the win
+ * conditions.
+ * <p>
+ * - While correct code is a goal, it is not the only goal ... We are looking
+ * for product level appropriate architecture, design, implementation
+ * and testing that meets the spec.
+ * Treat this code a component of a larger system (excluding
+ * the UI).
+ * <p>
+ * - Be sure to communicate a coherent functional design.
+ * <p>
+ * Questions are good....  if you need clarification of this
+ * description... better to ask than assume.
+ * <p>
+ * Be prepared to defend your choices and demonstrate your result.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
